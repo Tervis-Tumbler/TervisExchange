74e5ccda5a10a01d2d5c801e89e6129607df960c
ChrisMagnuson <hourglasssand@gmail.com>
2017-11-07 14:47:27 -0500

    Initial commit of Import-TervisExchangePSSession

---
+++ TervisExchange.psm1
@@ -0,0 +1,29 @@
+﻿function Import-TervisExchangePSSession {
+    [CmdletBinding()]
+    param ()
+
+    $Sessions = Get-PsSession |
+    Where ComputerName -eq "exchange2016.tervis.prv"
+    
+    $Sessions |
+    Where State -eq "Broken" |
+    Remove-PSSession
+
+    $Session = $Sessions |
+    Where State -eq "Opened" |
+    Select -First 1
+
+    if (-Not $Session) {
+        $FunctionInfo = Get-Command Get-ExchangeMailbox -ErrorAction SilentlyContinue
+        if ($FunctionInfo) {
+            Remove-Module -Name $FunctionInfo.ModuleName            
+        }
+        
+        $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://exchange2016.tervis.prv/PowerShell/       
+    }
+
+    $FunctionInfo = Get-Command Get-ExchangeMailbox -ErrorAction SilentlyContinue
+    if (-not $FunctionInfo) {
+        Import-Module (Import-PSSession $Session -DisableNameChecking -AllowClobber) -DisableNameChecking -Global -Prefix "Exchange"
+    }
+}
